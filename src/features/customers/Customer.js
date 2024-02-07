0006d814bd0321482ba422baf0cbb655696cdda2
anna-vit <sidorovahanna795@gmail.com>
2024-02-07 13:58:29 -0500

    Added API calls with Redux Thunks

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
-  console.log(customer, "customerName");
   return <h2>👋 Welcome, {customer}</h2>;
 }
 
